d1508a17362af73b52cf37f2bdf26038a5beded8
Yan Velasco <yancarlosduartevelasco@gmail.com>
2024-08-12 19:17:35 -0300

    feet: add AuthenticationManager to authenticate the user

--- src/main/java/com/yanvelasco/authapi/infra/security/SecurityConfigurations.java
+++ src/main/java/com/yanvelasco/authapi/infra/security/SecurityConfigurations.java
@@ -3,6 +3,8 @@ package com.yanvelasco.authapi.infra.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -24,6 +26,11 @@ public class SecurityConfigurations {
                         .anyRequest().authenticated()
                 )
                 .build();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
     }
 
 }
